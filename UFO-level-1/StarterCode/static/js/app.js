284bbffdbc6dab13f67261dfb93560ea10e55c34
jamesa820 <59709525+jamesa820@users.noreply.github.com>
2020-05-11 21:16:00 -0500

    Update app.js

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -1,1 +1,24 @@
+//pull data
+var tableData = data;
+
+// table variable
+
+var tbody = d3.select("tbody");
+
+  console.log(data);
+
+ //append rows 
+data.forEach(function(reportSightings) {
+  console.log(reportSightings);
+  var row = tbody.append("tr");
+
+  // append key value pairs
+  Object.entries(reportSightings).forEach(function([key, value]){
+    console.log(key, value);
+
+    // show text
+    var cell = row.append("td");
+    cell.text(value);
+        });
+});
 
