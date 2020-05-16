26b0bfd78f71930e67b274d5f1c0af67be9c4506
Brian Kennedy <bpkennedy@gmail.com>
2020-05-15 22:02:42 -0500

    d3-table Implement reset and filter of table when user types into filter

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -1,59 +1,42 @@
-// from data.js
-var tableData = data;
+const reportSightings = window.data;
+const tbodyToWipe = document.getElementById('ufo-table');
 
-// YOUR CODE HERE!
+function handleFilterButtonClick() {
+  const userFilterValue = d3.select('#datetime').property('value');
+  const filteredSightings = reportSightings.filter(reportSighting => reportSighting.datetime === userFilterValue);
+  resetTable();
+  filterReportSightingsIntoTable(filteredSightings);
+}
 
-var tbody = d3.select("tbody");
+function handleFilterInputKeyup(event) {
+  if (event.keyCode === 13) {
+    handleFilterButtonClick();
+  }
+}
 
-console.log(data);
+function resetTable() {
+  tbodyToWipe.innerHTML = `<thead>
+      <tr>
+        <th class="table-head">Date</th>
+        <th class="table-head">City</th>
+        <th class="table-head">State</th>
+        <th class="table-head">Country</th>
+        <th class="table-head">Shape</th>
+        <th class="table-head">Duration</th>
+        <th class="table-head">Comments</th>
+      </tr>
+    </thead>
+    <tbody></tbody>`;
+}
 
-// Loop through the data append table rows
-data.forEach(function(reportSightings) {
-  console.log(reportSightings);
-  var row = tbody.append("tr");
+function filterReportSightingsIntoTable(sightings) {
+  var tbody = d3.select('tbody');
+  sightings.forEach(sighting => {
+    const row = tbody.append('tr');
+    Object.entries(sighting).forEach(([key, value]) => {
+      row.append('td').text(value);
+    });
+  });
+}
 
-  // console.log sightings data
-  Object.entries(reportSightings).forEach(function([key, value]){
-    console.log(key, value);
-
-    // append cell values
-    var cell = row.append("td");
-    cell.text(value);
-        
-
-
-
-    // Get the value property of the input element
-    var button = d3.select("#filter-btn");
-
-    //  Select the form
-    var form = d3.select(".filter list-group-item");
-
-    // Create event handlers 
-    button.on("click", runEnter);
-    form.on("submit",runEnter);
-
-    // Complete the event handler function for the form
-    function runEnter() {
-
-      // Prevent the page from refreshing
-     d3.event.preventDefault();
-          
-      // Select the input element and get the raw HTML node
-      var inputElement = d3.select("#datetime");
-
-      // Get the value property of the input element
-      var inputValue = inputElement.property("value");
-
-      console.log(inputValue);
-      console.log(tableData);
-
-      var filteredData = tableData.filter(sightings => sightings.datetime === inputValue);
-
-      console.log(filteredData);
-
-    
-
-    };
-  });     
-});      
+filterReportSightingsIntoTable(reportSightings);
